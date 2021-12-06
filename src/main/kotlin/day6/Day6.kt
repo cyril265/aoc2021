0dfa4b1c18dda48fc395407eab1ca6ab5e76f10d
cyril <kpovolotsk@zoho.com>
2021-12-07 00:27:15 +0100

    fish it

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -8,46 +8,42 @@ val input = readToList("day6.txt")
     .map { it.toInt() }
 
 fun main() {
-    fishIt(256)
+    println(fishIt(80))
+    println(fishIt(256))
 }
 
-private fun fishIt(days: Int) {
+private fun fishIt(days: Int): Long {
     val fishArray = LongArray(7)
     input.forEach {
         fishArray[it]++
     }
-
     val newFishArray = LongArray(9)
 
     (1..days).forEach { _ ->
-        var waitingFishCount = 0L
-        var waitingNewFish = 0L
+        val first = tick(fishArray)
+        val second = tick(newFishArray)
 
-        fishArray.forEachIndexed { dayIndex, fishCount ->
-            if (dayIndex == 0) {
-                fishArray[0] -= fishCount
-                waitingFishCount += fishCount
-                waitingNewFish += fishCount
-            } else {
-                fishArray[dayIndex] -= fishCount
-                fishArray[dayIndex - 1] += fishCount
-            }
-        }
-
-        newFishArray.forEachIndexed { dayIndex, fishCount ->
-            if (dayIndex == 0) {
-                newFishArray[0] -= fishCount
-                waitingFishCount += fishCount
-                waitingNewFish += fishCount
-            } else {
-                newFishArray[dayIndex] -= fishCount
-                newFishArray[dayIndex - 1] += fishCount
-            }
-        }
-
-        fishArray[6] = waitingFishCount
-        newFishArray[8] = waitingNewFish
+        fishArray[6] = first.resetFishCount + second.resetFishCount
+        newFishArray[8] = first.resetNewFishCount + second.resetNewFishCount
     }
 
-    println(fishArray.sum() + newFishArray.sum())
+    return fishArray.sum() + newFishArray.sum()
 }
+
+fun tick(fishArray: LongArray): TickResult {
+    var waitingFishCount = 0L
+    var waitingNewFishCount = 0L
+    fishArray.forEachIndexed { dayIndex, fishCount ->
+        if (dayIndex == 0) {
+            fishArray[0] -= fishCount
+            waitingFishCount += fishCount
+            waitingNewFishCount += fishCount
+        } else {
+            fishArray[dayIndex] -= fishCount
+            fishArray[dayIndex - 1] += fishCount
+        }
+    }
+    return TickResult(waitingFishCount, waitingNewFishCount)
+}
+
+data class TickResult(val resetFishCount: Long, val resetNewFishCount: Long)
