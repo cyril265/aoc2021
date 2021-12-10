b796931837bd50c8b99a50e39e9ce37bf9ac9758
kpovolotskyy <kirill.povolotskyy@n4.de>
2021-12-10 17:25:59 +0100

    wip

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -3,30 +3,29 @@ package day9
 import readToList
 
 private val input = readToList("day9.txt")
-    .map { line -> line.map { char -> Character.getNumericValue(char) } }
-
-fun main() {
-//    println(part1())
-
-    val p2Input = input.map {
-        it.map { height ->
-            Height(height = height, valid = height != 9)
+    .map { line ->
+        line.map { char ->
+            val numericValue = Character.getNumericValue(char)
+            Height(height = numericValue, valid = numericValue != 9)
         }
     }
 
+fun main() {
+    val p2Input = HeightMatrix(input)
+//    val message = part1()
+//    println(message)
+
     val part2 = part2(p2Input)
     println(part2)
-
-//    val message = p2Input.map { line -> line.map { if (it.lowest) it.height.toString() else "0" } }
-//    println(message.joinToString(separator = "\n"))
 }
 
-private fun part2(input: List<List<Height>>): Long {
+private fun part2(input: HeightMatrix): Long {
+    part1()
     val basins = mutableListOf<Int>()
-    for ((listIndex, line) in input.withIndex()) {
-        for ((index, height) in line.withIndex()) {
-            if (height.lowest || !height.valid) continue
-            val size = setLowest(index, input, listIndex)
+    for ((columnIndex, line) in input.heightMatrix.withIndex()) {
+        val lowestPoints = line.withIndex().filter { (_, height) -> height.lowest }
+        for ((rowIndex, height) in lowestPoints) {
+            val size = setBasin(input, rowIndex, columnIndex)
             if (size > 0) {
                 basins.add(size)
                 println(size)
@@ -41,39 +40,38 @@ private fun part2(input: List<List<Height>>): Long {
 }
 
 
-private fun setLowest(
+private fun setBasin(
+    heightMatrix: HeightMatrix,
     columnIndex: Int,
-    heightMatrix: List<List<Height>>,
     lineIndex: Int
 ): Int {
-    val currentItem = heightMatrix.getOrNull(lineIndex)?.getOrNull(columnIndex)
-    if (currentItem == null || currentItem.lowest || !currentItem.valid) return 0;
+    val currentItem = heightMatrix.get(lineIndex, columnIndex)
+    if (currentItem == null || currentItem.basinPart || !currentItem.valid) return 0;
 
-    val line = heightMatrix[lineIndex]
-    val left = line.getOrNull(columnIndex - 1)
-    val right = line.getOrNull(columnIndex + 1)
-    val up = heightMatrix.getOrNull(lineIndex - 1)?.get(columnIndex)
-    val down = heightMatrix.getOrNull(lineIndex + 1)?.get(columnIndex)
+    val left = heightMatrix.get(lineIndex, columnIndex - 1)
+    val right = heightMatrix.get(lineIndex, columnIndex + 1)
+    val up = heightMatrix.get(lineIndex - 1, columnIndex)
+    val down = heightMatrix.get(lineIndex + 1, columnIndex)
 
     val lowest = arrayOf(left, right, down, up)
         .filterNotNull()
         .filter { it.valid }
-        .all { it.lowest || currentItem.height < it.height }
+        .all { it.basinPart || currentItem.height < it.height }
 
     var basinSize = 0
     if (lowest) {
-        currentItem.lowest = true
+        currentItem.basinPart = true
         basinSize += 1
-        basinSize += setLowest(columnIndex - 1, heightMatrix, lineIndex)
-        basinSize += setLowest(columnIndex + 1, heightMatrix, lineIndex)
-        basinSize += setLowest(columnIndex, heightMatrix, lineIndex - 1)
-        basinSize += setLowest(columnIndex, heightMatrix, lineIndex + 1)
+        basinSize += setBasin(heightMatrix, columnIndex - 1, lineIndex)
+        basinSize += setBasin(heightMatrix, columnIndex + 1, lineIndex)
+        basinSize += setBasin(heightMatrix, columnIndex, lineIndex - 1)
+        basinSize += setBasin(heightMatrix, columnIndex, lineIndex + 1)
     }
     return basinSize
 }
 
 private fun part1(): Int {
-    val lowestPoints = mutableListOf<Int>()
+    val lowestPoints = mutableListOf<Height>()
 
     for ((listIndex, line) in input.withIndex()) {
         for ((index, height) in line.withIndex()) {
@@ -84,14 +82,14 @@ private fun part1(): Int {
         }
     }
 
-    return lowestPoints.sumOf { it + 1 }
+    return lowestPoints.sumOf { it.height + 1 }
 }
 
 private fun isLowest(
     index: Int,
-    convertedInput: List<List<Int>>,
+    convertedInput: List<List<Height>>,
     lineIndex: Int,
-    height: Int
+    currentElement: Height
 ): Boolean {
     val line = convertedInput[lineIndex]
     val prev = line.getOrNull(index - 1)
@@ -100,8 +98,23 @@ private fun isLowest(
         ?.get(index)
     val up = convertedInput.getOrNull(lineIndex + 1)
         ?.get(index)
-    val lowest = arrayOf(prev, next, down, up).filterNotNull().all { height < it }
-    return lowest
+    val lowest = arrayOf(prev, next, down, up).filterNotNull().all { currentElement.height < it.height }
+    if (lowest) {
+        currentElement.lowest = true
+        return true
+    }
+    return false
 }
 
-private data class Height(val height: Int, var lowest: Boolean = false, val valid: Boolean)
+private data class Height(
+    val height: Int,
+    var lowest: Boolean = false,
+    val valid: Boolean,
+    var basinPart: Boolean = false
+)
+
+private data class HeightMatrix(val heightMatrix: List<List<Height>>) {
+    fun get(lineIndex: Int, columnIndex: Int): Height? {
+        return heightMatrix.getOrNull(lineIndex)?.getOrNull(columnIndex)
+    }
+}
